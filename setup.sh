508574373c1a1cca42508d0869c90aaf87ac55a8
Wolfgramm, Jens <jens.wolfgramm@damantec.de>
2023-12-30 10:32:06 +0100

    changes

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install software-properties-common -y
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt update
+sudo apt install ansible -y
+sudo apt install python3-argcomplete -y
+
+ansible-pull -U https://github.com/WolfgrammJ/ansible-htpc.git setup.yml --checkout main
